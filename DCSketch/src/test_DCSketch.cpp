3b6d67f3b496ace2d968d2c01fba9a499749eee9
XUN <1056054431@qq.com>
2021-11-08 22:02:48 +0800

    modify test files

--- DCSketch/src/test_DCSketch.cpp
+++ DCSketch/src/test_DCSketch.cpp
@@ -20,10 +20,10 @@ int main()
 #define OUTPUT_SKETCH 1
 //#define OUTPUT_REAL_DISTRIBUTION 1
     DCSketch dcsketch;
-    string dataset = "CAIDA";
+    string dataset = "MAWI";
     //string filename = "imc_merge_0000";
-    string filename = "CAIDA_frag_0000";
-    //string filename = "pkts_frag_00000";
+    //string filename = "CAIDA_frag_0000";
+    string filename = "pkts_frag_00000";
     PCAP_SESSION session(dataset,filename);
     IP_PACKET cur_packet;
     string srcip,dstip;
@@ -103,12 +103,60 @@ void write_sketch(string dataset,string filename,DCSketch& dcsketch)
     {
         ofile_hand << dcsketch.layer1.get_spread(i) << endl;
     }
-
     uint32_t layer2_len = dcsketch.layer2.HLL_num;
     ofile_hand<<"layer2: "<<layer2_len << endl;
     for(size_t i = 0;i < layer2_len;i++)
     {
         ofile_hand << dcsketch.layer2.get_spread(i) << endl;
+    }
+    ofile_hand.close();
+
+
+    ofile_hand = ofstream(ofile_path + filename.substr(filename.size() - 4) + "sketch_dist.txt");
+    if(!ofile_hand)
+    {
+        cout<<"fail to open files."<<endl;
+        return;
+    }
+    unordered_map<uint32_t,uint32_t> HLL_dist;
+    set<uint32_t> possible_vals;
+    for(size_t i = 0;i < dcsketch.layer2.HLL_num;i++)
+    {
+        uint32_t cur_val = dcsketch.layer2.get_spread(i);
+        if(HLL_dist.find(cur_val) != HLL_dist.end())
+            HLL_dist[cur_val]++;
+        else
+            HLL_dist.insert(pair<uint32_t,uint32_t>(cur_val,1));
+    }
+    possible_vals.clear();
+    for(auto iter = HLL_dist.begin();iter != HLL_dist.end();iter++)
+    {
+        possible_vals.insert(iter->first);
+    }
+    ofile_hand<<"HLL value distribution"<<endl;
+    for(auto hllval : possible_vals)
+    {
+        ofile_hand << hllval << " " << HLL_dist[hllval] << endl;
+    }
+
+    unordered_map<uint32_t,uint32_t> Bitmap_dist;
+    for(size_t i = 0;i < dcsketch.layer1.bitmap_num;i++)
+    {
+        uint32_t cur_val = dcsketch.layer1.get_spread(i);
+        if(Bitmap_dist.find(cur_val) != Bitmap_dist.end())
+            Bitmap_dist[cur_val]++;
+        else
+            Bitmap_dist.insert(pair<uint32_t,uint32_t>(cur_val,1));
+    }
+    possible_vals.clear();
+    for(auto iter = Bitmap_dist.begin();iter != Bitmap_dist.end();iter++)
+    {
+        possible_vals.insert(iter->first);
+    }
+    ofile_hand<<"Bitmap value distribution"<<endl;
+    for(auto hllval : possible_vals)
+    {
+        ofile_hand << hllval << " " << Bitmap_dist[hllval] << endl;
     }
     ofile_hand.close();
 }
@@ -180,42 +228,3 @@ void write_real_distribution(string dataset,string filename,DCSketch& dcsketch)
 
 
 
-// unordered_map<uint32_t,uint32_t> HLL_dist;
-// for(size_t i = 0;i < dcsketch.layer2.HLL_num;i++)
-// {
-//     uint32_t cur_val = dcsketch.layer2.get_spread(i);
-//     if(HLL_dist.find(cur_val) != HLL_dist.end())
-//         HLL_dist[cur_val]++;
-//     else
-//         HLL_dist.insert(pair<uint32_t,uint32_t>(cur_val,1));
-// }
-// keys.clear();
-// for(auto iter = HLL_dist.begin();iter != HLL_dist.end();iter++)
-// {
-//     keys.insert(iter->first);
-// }
-// ofile_hand<<"HLL value distribution"<<endl;
-// for(auto hllval : keys)
-// {
-//     ofile_hand << hllval << " " << HLL_dist[hllval] << endl;
-// }
-
-// unordered_map<uint32_t,uint32_t> Bitmap_dist;
-// for(size_t i = 0;i < dcsketch.layer1.bitmap_num;i++)
-// {
-//     uint32_t cur_val = dcsketch.layer1.get_spread(i);
-//     if(Bitmap_dist.find(cur_val) != Bitmap_dist.end())
-//         Bitmap_dist[cur_val]++;
-//     else
-//         Bitmap_dist.insert(pair<uint32_t,uint32_t>(cur_val,1));
-// }
-// keys.clear();
-// for(auto iter = Bitmap_dist.begin();iter != Bitmap_dist.end();iter++)
-// {
-//     keys.insert(iter->first);
-// }
-// ofile_hand<<"Bitmap value distribution"<<endl;
-// for(auto hllval : keys)
-// {
-//     ofile_hand << hllval << " " << Bitmap_dist[hllval] << endl;
-// }
